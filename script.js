03005367f40a1a7189b3ab9be504d5870ceacb13
Kertesbacsi <122312208+Kertesbacsi@users.noreply.github.com>
2025-08-07 13:59:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -159,3 +159,12 @@ window.addEventListener("load", revealOnScroll);
 
 // Alapértelmezett szöveg betöltése
 aboutText.textContent = aboutTexts[0];
+
+
+// Hamburger menü mobilra
+const menuToggle = document.getElementById('menuToggle');
+const navbar = document.getElementById('navbar');
+
+menuToggle.addEventListener('click', () => {
+  navbar.classList.toggle('show');
+});
